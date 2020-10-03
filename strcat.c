549ceeb10c0e9c3d307f033337e44115bacc936f
ZT-715 <gabes.zanella@gmail.com>
2020-10-03 14:46:30 -0300

    to do

--- strcat.c
+++ strcat.c
@@ -1,10 +1,27 @@
-#define concat(x,y) x##y 
+char strcat(char s[],char t[]);
 
-/* strcat -> get two strings and put the second after the first (s,t) = s+t */
-void strcat(char *s, char *t){
+void main(void){
   
-  char newStr[] = concat(*s,*t);
+  char first[] = "aaa ";
+  char second[] = "bla.";
 
-  s = newStr;  
+  strcat(first,second);
+
 }
 
+/* strcat -> get two strings and put the second after the first (s,t) = s+t */
+ char strcat(char *s, char *t){
+  
+  int i;
+
+  char newStr[sizeof(s)+sizeof(t)-1];
+
+  for(i=0; s[i] =! '\0'; i++){
+    newStr[i] = s[i];
+  }
+  for(i=0; t[i] =! '\0'; i++){
+    newStr[sizeof(s)+i] = t[i];
+  }
+  
+  s = newStr;  
+}
