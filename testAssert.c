996ddb96ac1b56e2c3e24b650680b5f4416d198f
zt <gabes.zanella@gmail.com>
2022-02-17 23:36:32 -0300

    test(assert): playing around w/ assert to test code

--- testAssert.c
+++ testAssert.c
@@ -1,40 +1,40 @@
-//Trying out the use of <assert.h>
+//Trying out the use of <assert.h> and atoi()
 
 //#define NDEBUG
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-static int number = 0;
-static int assertTest();
+int main(int argc, char *argv[])
+{
+    int number;
 
-int main(int argc, char *argv[]) {
-    assert(number == 0);
+    if (argc == 2)
+    {
+        if (atoi(argv[1]))
+        {
+#ifndef NDEBUG // shows if the input accepted was integers only
+            int i = 0;
 
-    if (argc < 2) {
-        fprintf(stderr, "No argument, use 0 or non-zero number as argument.\n");
-        return(EXIT_FAILURE);
+            while (argv[1][++i] != '\0')
+            {
+                assert('0'<= argv[1][i] && argv[1][i] <= '9');     //asserts ASCII number  '0 <= x <= 9'
+            }
+#endif
+            number = atoi(argv[1]);
+            printf("number: %i\n", number);
+
+            return EXIT_SUCCESS;
+        }
+        else
+        {
+            fprintf(stderr, "Use a single non-zero integer number as argument.\n");
+            return EXIT_FAILURE;
+        }
     }
-    else {
-        number = atoi(argv[1]);
+    else
+    {
+        fprintf(stderr, "No argument, use a single non-zero number as argument.\n");
+        return EXIT_FAILURE;
     }
-    
-    assert(assertTest()==0);
-
-    assertTest();
-
-    printf("end\n");
-    return(EXIT_FAILURE);
-}
-
-static int assertTest() {
-    
-    #ifdef NDEBUG
-    #undef NDEBUG
-    #include <assert.h>
-    printf("assert %d == 1.\n", number);
-    assert(number == 1);
-    #endif // !NDEBUG
-   
-    return number;
 }
