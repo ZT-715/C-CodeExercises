098f7b1cdd38a24f8dabd2b55329d2f6b53e324d
Gabriel S. Zanella <gabes.zanella@gmail.com>
2022-02-19 18:27:41 -0300

    add(count_occurrences): input handling

--- count_occurrences.c
+++ count_occurrences.c
@@ -1,6 +1,26 @@
 // Write a program to count blanks, tabs, and newlines in a file.
 
-int main(void)
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <err.h>
+
+int main(int argc, char *argv[])
 {
+   if (argc != 2)
+    {
+        fprintf(stderr, "Use a text file as argument.\n");
+        return EXIT_FAILURE;
+    }
+
+    FILE *fp;
+
+    if ((fp = fopen(argv[1], "r" )) == NULL)
+    {
+        err(errno, "%s", argv[1]);
+    }
+
+
+
     return 0;
 }
