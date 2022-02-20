000b62de2461ae6ad9948cbb2805f869bf536ea1
Gabriel S. Zanella <gabes.zanella@gmail.com>
2022-02-19 21:38:22 -0300

    feat(count_uccurrences): finished

--- count_occurrences.c
+++ count_occurrences.c
@@ -1,26 +1,55 @@
-// Write a program to count blanks, tabs, and newlines in a file.
+// Write a program to count blanks, tabs, and newlines in a text file:
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
 #include <err.h>
 
-int main(int argc, char *argv[])
-{
-   if (argc != 2)
-    {
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
         fprintf(stderr, "Use a text file as argument.\n");
         return EXIT_FAILURE;
     }
 
     FILE *fp;
 
-    if ((fp = fopen(argv[1], "r" )) == NULL)
-    {
+    if ((fp = fopen(argv[1], "r")) == NULL) {
         err(errno, "%s", argv[1]);
     }
 
+    volatile unsigned long num_blanks;
+    volatile unsigned long num_tabs;
+    volatile unsigned long num_lines;
 
+    unsigned int current;
 
-    return 0;
+    while (current != EOF) {
+        current = fgetc(fp);
+
+        switch (current) {
+            case ' ':
+                num_blanks++;
+                break;
+
+            case '\t':
+                num_tabs++;
+                break;
+
+            case '\n':
+                num_lines++;
+                break;
+
+            default:
+                break;
+        }
+
+    }
+
+    fclose(fp);
+
+    printf("Number of blanks:   %li \n", num_blanks);
+    printf("Number of tabs:     %li \n", num_tabs);
+    printf("Number of lines:    %li \n", num_lines);
+
+    return EXIT_SUCCESS;
 }
